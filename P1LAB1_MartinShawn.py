6def3b33c0bdb90cac992bc543ade1273c51dd59
martins8173 <martins8173@student.faytechcc.edu>
2025-02-27 21:07:40 -0500

    Add files via upload

---
+++ P1LAB1_MartinShawn.py
@@ -0,0 +1,9 @@
+#Shawn martin
+#02/27/2025
+#P1LAB1
+#Introduction to Python
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print('hello,', first_name, last_name,'and welcome to CTI-110') 
